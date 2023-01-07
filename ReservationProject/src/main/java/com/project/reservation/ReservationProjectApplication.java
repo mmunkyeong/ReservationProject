e75debd4ec01367666aeaa15915cca39f3655828
mmunkyeong <62290451+mmunkyeong@users.noreply.github.com>
2023-01-07 22:57:31 +0900

    로그인, 회원가입 mustache 적용

--- ReservationProject/src/main/java/com/project/reservation/ReservationProjectApplication.java
+++ ReservationProject/src/main/java/com/project/reservation/ReservationProjectApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.project"})
 public class ReservationProjectApplication {
 
 	public static void main(String[] args) {
